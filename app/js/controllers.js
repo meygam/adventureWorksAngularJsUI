73b5d6718bdc420af38b06122eb8c2d390ca3182
saravana kumar periyasamy <satavanakumar.periyasamy@trane.com>
2013-10-18 19:09:27 -0500

    Added the twitter bootstrap css

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,4 +8,19 @@ angular.module('myApp.controllers', []).
   }])
   .controller('MyCtrl2', [function() {
 
-  }]);
+  } ]);
+
+function productCategoryController($scope, $http) {
+    $scope.productCatogeries = [];
+
+    $http.get("http://localhost:53583/api/productcategory").then(
+        function (result) {
+            //success
+            angular.copy(result.data, $scope.productCatogeries);
+        },
+        function () {
+            //error
+            alert("Could not load Product Categories");
+        }
+    );
+  }
